70108d01ac0a6455ccc7efe11480bbe9ed15ef84
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-07-20 18:34:14 -0300

    [class 8]

--- raycasting/raycast.js
+++ raycasting/raycast.js
@@ -10,16 +10,36 @@ class Map {
         this.grid = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
+    }
+    render() {
+        //increases the line
+        for (var i = 0 ; i < MAP_ROWS; i++) { 
+            //increases the column
+            for (var j = 0 ; j < MAP_COLS; j++) { 
+                //sets tileX as the column number * the tile size
+                var tileX = j * TILE_SIZE;
+                //sets tileY as the line number * the tile size
+                var tileY = i * TILE_SIZE;
+                //sets 1 as black and 0 as white
+                var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff";
+                //sets a stroke color
+                stroke("#222");
+                //uses the tileColor as setting for filling the image
+                fill(tileColor);
+                //creates a rectangle with the infos setted above
+                rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+            }
+        }
     }
 }
 
@@ -37,5 +57,6 @@ function update() {
 //TODO: draw all objs frame by frame
 function draw() {
     update();
+
     grid.render();
 }
